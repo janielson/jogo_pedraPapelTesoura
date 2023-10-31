0022068d69ff1055d39cf490988605e99bf10bbb
JScript <43323432+janielson@users.noreply.github.com>
2023-10-31 07:17:45 -0300

    irei ajustar o background de fundo

---
+++ scripts.js
@@ -0,0 +1,23 @@
+
+
+
+
+// Lista de imagens
+const imagens = [".assets/background1.jpg", ".assets/background2.jpg", ".assets/background3.jpg", ".assets/background4.jpg", ".assets/background5.jpg", 
+".assets/background6.jpg", ".assets/background7.jpg",".assets/background8.jpg","assets/background9.jpg",".assets/background10.jpg"];
+
+// Índice da imagem atual
+let indiceImagemAtual = 0;
+
+
+// Atualiza a imagem a cada 5 segundos
+setInterval(() => {
+    // Obtém a nova imagem
+    const novaImagem = imagens[indiceImagemAtual];
+
+    // Altera o background de imagem
+    document.body.style.backgroundImage = `url(${novaImagem})`;
+
+    // Avança o índice da imagem
+    indiceImagemAtual = (indiceImagemAtual + 1) % imagens.length;
+}, 5000);
