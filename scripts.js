c1e2851e7d3bd84a0c84cbbbcff5fc52db4d659e
JScript <43323432+janielson@users.noreply.github.com>
2023-11-07 07:31:59 -0300

    ajustei as variaveis

--- scripts.js
+++ scripts.js
@@ -1,26 +1,28 @@
 
 // VARIAVEIS GLOBAIS
-var iniciarJogoButton = document.querySelector(".button");
-var pedraButton = document.querySelector(".content__jogo__imgs__button__one");
-var papelButton = document.querySelector(".content__jogo__imgs__button__tow");
-var tesouraButton = document.querySelector(".content__jogo__imgs__button__three");
-var buttonGame = document.querySelectorAll(".button__game");
-var jogadasRestantes = 10;
-var suaPontuacao = 0;
-var pontuacaoInteligencia = 0;
+let iniciarJogoButton = document.querySelector(".button");
+let pedraButton = document.querySelector(".content__jogo__imgs__button__one");
+let papelButton = document.querySelector(".content__jogo__imgs__button__tow");
+let tesouraButton = document.querySelector(".content__jogo__imgs__button__three");
+let buttonGame = document.querySelectorAll(".button__game");
+let myResult = document.querySelector(".content-jogo-resut-my-resuts");
+let inteligenciaResult = document.querySelector(".content-jogo-resut-inteligence");
+
+
+
+let jogadasRestantes = 10;
+let suaPontuacao = 0;
+let pontuacaoInteligencia = 0;
 
 // EVENTOS DE CLIQUE
-// buttonGame.addEventListener("click", jogarPedra);
 iniciarJogoButton.addEventListener("click", openSectionOfPlay);
-// pedraButton.addEventListener("click", jogarPedra);
-// papelButton.addEventListener("click", jogarPapel);
-// tesouraButton.addEventListener("click", jogarPedra);
+
 
 for(let i = 0 ;  i < buttonGame.length ; i ++) {
   buttonGame[i].addEventListener("click", () => {
       
    let escolhaDoUsuario = buttonGame[i].querySelector(".content-jogo-imgs__pedra").innerHTML;
-   var escolhaDaInteligencia = Math.floor(Math.random() * 3);
+   let escolhaDaInteligencia = Math.floor(Math.random() * 3);
 
    
   if (escolhaDaInteligencia == 0) {
@@ -64,11 +66,13 @@ for(let i = 0 ;  i < buttonGame.length ; i ++) {
 }
 
   
-  console.log("sua Pontuação "  + suaPontuacao);
-  console.log("sua Escolha "  + escolhaDoUsuario);
-  console.log("");
-  console.log("pontuação da inteligencia " + pontuacaoInteligencia);
-  console.log("escolha da inteligencia " + escolhaDaInteligencia); 
+  // console.log("sua Pontuação "  + suaPontuacao);
+  myResult.innerHTML = suaPontuacao;
+  // console.log("sua Escolha "  + escolhaDoUsuario);
+  // console.log("");
+  // console.log("pontuação da inteligencia " + pontuacaoInteligencia);
+  inteligenciaResult.innerHTML = pontuacaoInteligencia;
+  // console.log("escolha da inteligencia " + escolhaDaInteligencia); 
    
   if(suaPontuacao > pontuacaoInteligencia){ 
     console.log("Você venceu");
@@ -89,45 +93,14 @@ for(let i = 0 ;  i < buttonGame.length ; i ++) {
 
 }
 
-
 // FUNÇOES DA APLICAÇÃO
 function openSectionOfPlay() {
 
-  var secaoDeJogos = document.querySelector(".content-jogo");  
+  let secaoDeJogos = document.querySelector(".content-jogo");  
   secaoDeJogos.style.display = "block";
   secaoDeJogos.scrollIntoView({ behavior: "smooth" });
 }
 
 
 
-//crie uma função que a cada escolha do usuario entre pedra,papel ou tesoura, o numeros de jogadas diminua menos 1 e a pontuação do usuario aumente 1
-function jogarPedra() {
 
-
- 
-
-  
-
-  var escolhaDoUsuario = document.querySelector(".button__game");   
-  var elementFilho = escolhaDoUsuario.querySelector(".content-jogo-imgs__pedra").innerHTML;
-
-  
-  console.log(elementFilho);
-
-  console.log("codigo primera verificação " +  escolhaDoUsuario)
-
-
-
-
-
-
-  }
-     //  pontuacaoInteligencia e suaPontuacao exibir na tela 
-
-   
-//       jogadasRestantes.innerHTML = "Jogadas restantes: " + jogadasRestantes;
-//   pontuacaoDoUsuario.innerHTML = "Sua pontuação: "
-
-
-// var jogadasRestantes = document.querySelector(".play_rounds");
-// var pontuacaoDoUsuario = document.querySelector(".content-jogo-resut-my-resuts");
