df6fcd2a93a220ad7967023e1719edc1d9a48c51
JScript <43323432+janielson@users.noreply.github.com>
2024-01-13 17:58:45 -0300

    funcionalidade de codigo javascript criado.

--- scripts.js
+++ scripts.js
@@ -1,33 +1,37 @@
-
-//paragrafos para serem execultados 
-const sejaBemVindo = document.querySelector(".ola-seja-bem-vindo")
-
-
-
-// audios para serem execultados
-// const audioSejaBemVindo = document.querySelector(".audio__seja__bem__vindo")
-
-
-
-
-
-
-
-
-// funçoes do programa
-
-function typewriter (paragrafos) {
-   let textoArray = paragrafos.innerHTML.split('')
-   paragrafos.innerHTML = ''
-   textoArray.forEach((letra, index) => {
-    setTimeout(function(){
-        paragrafos.innerHTML += letra
-
-    },75 * index)
-    
-   });
-
+function speak(text) {
+    let utterance = new SpeechSynthesisUtterance(text);
+    utterance.lang = 'pt-BR'; // Definir o idioma para português do Brasil
+    speechSynthesis.speak(utterance);
 }
 
+function typeWriter(text, n, element, callback) {
+    if (n < text.length) {
+        element.innerHTML = text.substring(0, n+1);
+        setTimeout(function() {
+            typeWriter(text, n+1, element, callback);
+        }, 100);
+    } else if (callback) {
+        setTimeout(callback, 1500);
+    }
+}
 
-typewriter(sejaBemVindo)
+function startTyping(dialogues) {
+    if (dialogues.length > 0) {
+        let dialogue = dialogues.shift();
+        let element = document.querySelector('.balao-de-dialagos p');
+        typeWriter(dialogue, 0, element, function() {
+            speak(dialogue);
+            startTyping(dialogues);
+        });
+    }
+}
+
+let dialogues = [
+    'Olá, seja bem-vindo!!!',
+    'Pronto para jogar Jokenpô?',
+    'Escolha Pedra, Papel ou Tesoura e boa sorte!'
+];
+
+document.addEventListener('DOMContentLoaded', function() {
+    startTyping(dialogues);
+});
