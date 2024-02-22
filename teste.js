4d243d08e17cf46ef279b1b330966a3f217b660e
Thiago Silva <thiagojklb@gmail.com>
2024-02-22 16:18:31 -0300

    modificando a tabela

---
+++ teste.js
@@ -0,0 +1,11 @@
+const listaDeItens = document.querySelector('.teste');
+
+
+let container = '<div>teste</div>'
+
+for(let index = 0 ; index < 4 ; index++) {
+
+listaDeItens.innerHTML += container;
+
+
+}
