2b990fac3d1c3dd913f3028d26ed04dd14656004
JScript <43323432+janielson@users.noreply.github.com>
2024-02-29 21:19:55 -0300

    fazendo exercicio javascript

--- arrays.js
+++ arrays.js
@@ -1,64 +1,23 @@
-// 1 - Para entender arrays precisamos saber de duas coisas.
-// o que sao os indices de um array e o que tem dentro do array exemplo
+const estudantes = [
+    { nome: "Ana", nota: 7.5 },
+    { nome: "Beto", nota: 5.0 },
+    { nome: "Carlos", nota: 9.0 },
+    { nome: "Diana", nota: 6.0 }
+];
 
-// criei um array com 3 elementos
-// let array = ["pedro" , "ana", "Joao", 10,true,3.00];
+let notaMaiorQueSete = estudantes.filter((alunos) => {
+return alunos.nota >= 7 
+})
 
-// o indice de um array sempre eu digo sempre começa no 0
-// no nosso caso temos um array com 3 elementos ou seja 
-// temos indices 0 1 e 2 esses sao os indices do array
-// o que tem dentro dos indices sao os tipos de dados que colocamos
-// dentro do array, ou seja string number boolean double etc
+ let notaAlunoAprovado = notaMaiorQueSete.map((objeto) => {
 
-
-// precisamos separar o que sao indices e o que sao elementos 
-// dentro dos indices. No nosso caso esse array possui 3 elementos
-// que sao "pedro" , "ana", "Joao"
-// pedro esta no indice 0
-// ana esta no indice 1
-// e joao esta no indice 2
-// como eu acesso esses elementos? atraves dos indices
-
-// let elementoZero = array[0];
-//console.log(elementoZero) // imprimira "pedro"
-
-// quando eu faço array[0] eu estou acessando o indice 0
-// que contem o elemento "pedro" e estou guardando esse elemento na 
-// variavel elementoZero e exibindo na tela 
-
-
-
-let produto1 = { nome: "Shampo", valor: 6.00 };
-let produto2 = { nome: "Chocolate", valor: 10.00 };
-let produto3 = { nome: "Pilha", valor: 12.00 };
-
-let listaDeProdutos = [produto1, produto2, produto3];
-
-
-console.log("teste")
-
-
-
-
-function retornaSomaProdutos(listaDeProdutos) {
-    let soma = 0;
-    for (let i = 0; i < listaDeProdutos.length; i++) {
-
-        soma += listaDeProdutos[i].valor;
-    }
-    return soma;
-}
-
-function imprimeDaDosDoProduto(listaDeProdutos) {
-    let soma = retornaSomaProdutos(listaDeProdutos);
+    return objeto.nota;
      
-     console.log("_________ Lista de Produtos __________")
-    for (let i = 0; i < listaDeProdutos.length; i++) {
-
-       console.log(listaDeProdutos[i].nome)
-    }
-    console.log("A soma total dos produtos é R$ " + soma.toFixed(2));
-}
+ } );
 
 
-console.log(imprimeDaDosDoProduto(listaDeProdutos));
+let media = notaMaiorQueSete.reduce((acc, element, index) => {
+    
+});
+
+
