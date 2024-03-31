01f8f0870595f5b4ca0105da65dd7cf865965351
JScript <43323432+janielson@users.noreply.github.com>
2024-03-30 22:50:53 -0300

    criando uma tela de login para o usuario entra no jogo

--- scriptteste.js
+++ scriptteste.js
@@ -1,5 +1,8 @@
-document.getElementById('open_btn').addEventListener('click', function () {
-  document.getElementById('sidebar').classList.toggle('open-sidebar');
+const inputText = document.querySelector('.container-login__senha');
+
+inputText.addEventListener('input', (event) => {
+  const value = event.target.value;
+  const regex = /[^0-9]/g; // regex que identifica caracteres que não sejam números
+  const newValue = value.replace(regex, ''); // remove caracteres não numéricos da string
+  event.target.value = newValue; // atualiza o valor do campo com a string modificada
 });
-
-
